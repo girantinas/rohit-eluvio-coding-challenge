a2b06531970ecc1fd9e5b8a16babcfe607f7b271
girantinas <rohaga@berkeley.edu>
2021-04-09 02:52:16 -0400

    fixed bugs

--- main.c
+++ main.c
@@ -1,13 +1,26 @@
 /* Returns the Longest Common Strand from a list of files named "sample.1", "sample.2", etc. */
+#define uchar unsigned char
+#define min(a,b) ((a)<(b)?(a):(b))
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-/* Compares strings given pointers to them. */
-int pstrcmp(const void *a, const void *b) {
-    return strcmp((const char *)*(char **)a, (const char *)*(char **)b);
-}
+//Global Variables, Useful for qsort functions.
+uchar* TOTAL_STRING; // concatString in main
+int SUM_OF_LENS; // sumOflens in main
 
+/* Helper struct which contains all the info from the files. */
+typedef struct {
+    // Array of all the different bytestrings
+    uchar **strs;
+    // Array of lengths of each char array
+    int *lens;
+} FileInfo;
+
+int lcp(char *a, char *b);
+int pstrcmp(const void *a, const void *b);
+FileInfo *readFiles(int numFiles);
+int findInterval(int num, int *intervalStarts, int numIntervals);
  
 /* Returns the longest common prefix of two strings. */
 int lcp(char *a, char *b) {
@@ -20,28 +33,44 @@ int lcp(char *a, char *b) {
     return i;
 }
 
-/* Reads a number of output files and returns an array of strings reproduced from the files. */
-char **readFiles(int numFiles) {
+/* Compares uchar arrays given pointers to them. */
+int pstrcmp(const void *a, const void *b) {
+    int len1 = SUM_OF_LENS - (*(uchar **)a - TOTAL_STRING);
+    int len2 = SUM_OF_LENS - (*(uchar **)b - TOTAL_STRING);
+    int cmp = memcmp((uchar *) *(uchar **)a, (uchar *) *(uchar **)b, min(len1, len2));
+    if (cmp == 0) {
+        if (len2 > len1) { return 1; }
+        else if (len1 > len2) { return -1; }
+        else { return 0; }
+    }
+    return cmp;
+}
+
+/* Reads a number of output files and returns an array of chars reproduced from the files. */
+FileInfo *readFiles(int numFiles) {
     char buffer[FILENAME_MAX];
-    char **strs = (char**)malloc(numFiles*sizeof(char*));
-    
+    uchar **strs = (uchar**)malloc(numFiles*sizeof(uchar*));
+    int *lens = (int *)malloc(numFiles*sizeof(int));
     for (int i = 0; i < numFiles; ++i) {
         sprintf(buffer, "sample.%d", i + 1); //Files are 1-indexed...
-        FILE* file = fopen(buffer, "r");
+        FILE* file = fopen(buffer, "rb");
 
         fseek(file, 0, SEEK_END);
-        int size = ftell(file);
+        size_t size = ftell(file);
         fseek(file, 0, SEEK_SET);
 
-        char* currStr = (char*)malloc(size*sizeof(char) + 1);
-        fread(currStr, sizeof(char), size, file);
-        currStr[size] = 0;
+        uchar* currStr = (uchar*)malloc(size*sizeof(uchar));
+        fread(currStr, sizeof(uchar), size, file);
 
+        lens[i] = size;
         strs[i] = currStr;
         fclose(file);
-        free(currStr);
     }
-    return strs;
+    FileInfo *info;
+    info = malloc(sizeof(FileInfo));
+    info->strs = strs;
+    info->lens = lens;
+    return info;
 }
 
 /* Finds which numbered interval a given number is in.*/
@@ -53,31 +82,29 @@ int findInterval(int num, int *intervalStarts, int numIntervals) {
     return --i;
 }
 
-int main(int argc, char **argv) {
-    if (argc < 0) {
-        return -1;
-    }
-    int NUM_FILES = atoi(argv[1]);
-    printf("%d", NUM_FILES);
+int main() {
+    int NUM_FILES;
+    printf("How many files would you like to scan? \n");
+    scanf("%d", &NUM_FILES);
     if (NUM_FILES < 2) {
-        printf("ERROR: Not enough files to find longest strand. \n");
+        printf("ERROR: Invalid input. \n");
         return -1;
     }
-    char **strs = readFiles(NUM_FILES);
+    FileInfo *info = readFiles(NUM_FILES);
+    unsigned char **strs = info->strs;
+    int *lens = info->lens;
     //Contains the index of the start of each string in the concatenated string.
     int *starts = (int*)malloc(NUM_FILES*sizeof(int));
     int sumOflens = 0;
     for (int i = 0; i < NUM_FILES; ++i) {
         starts[i] = sumOflens;
-        sumOflens += strlen(strs[i]);
+        sumOflens += lens[i];
     }
+    uchar **arrayPointers = (uchar**)malloc((sumOflens)*sizeof(uchar*));
+    uchar *concatString = (uchar*)malloc((sumOflens)*sizeof(uchar));
 
-    char **arrayPointers = (char**)malloc((sumOflens)*sizeof(char*));
-    char *concatString = (char*)malloc((sumOflens)*sizeof(char));
-
-    concatString[0] = 0;
     for (int i = 0; i < NUM_FILES; ++i) {
-        strcat(concatString, strs[i]);
+        memcpy(concatString + starts[i], strs[i], lens[i]);
     }
 
     // Set up array pointers (representing suffixes)
@@ -85,9 +112,12 @@ int main(int argc, char **argv) {
         arrayPointers[i] = &(concatString[i]);
     }
 
+    //Store these things in global vars for use qsort functions
+    TOTAL_STRING = concatString;
+    SUM_OF_LENS = sumOflens;
     //Sort the suffix array
-    qsort(arrayPointers, sumOflens, sizeof(char *), pstrcmp);
-
+    qsort(arrayPointers, sumOflens, sizeof(uchar **), pstrcmp);
+    
     // Length of longest common substring.
     int lcslen = 0;
     // Which numbered strings are the ones which share the lcs.
@@ -120,9 +150,13 @@ int main(int argc, char **argv) {
     if (lcsstr1 == -1) {
         printf("No Common Strand.\n");
     } else {
-        printf("Longest Common Strand found: \n File 1: sample.%d ; \t Strand Offset: %d \
-                \n File 2: sample.%d ; \t Strand Offset: %d",
-               lcsstr1, lcssufpos1, lcsstr2, lcssufpos2);
+        printf("Longest Common Strand found: Length %d \n File 1: sample.%d\t Strand Offset: %d \
+                \n File 2: sample.%d\t Strand Offset: %d \n",
+               lcslen, lcsstr1, lcssufpos1, lcsstr2, lcssufpos2);
     }
-
+    free(strs);
+    free(starts);
+    free(arrayPointers);
+    free(concatString);
+    free(info);
 }
